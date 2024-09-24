4f3daea031157dbf900266e96616e791c392b334
jehanpark <5071pjh@naver.com>
2024-09-24 20:29:28 +0900

    introUpdate

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Nav from "../Components/Nav";
+import Intro from "../Components/Intro";
 
 const Home = () => {
   return (
     <div>
+      <Intro />
       <Nav />
     </div>
   );
