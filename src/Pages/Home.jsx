df8aaa73e68f81ec9c73b94a7b52842cad953494
kim5537 <fudgml778@naver.com>
2024-09-27 18:58:57 +0900

    icon-set1

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Nav from "../Components/Nav";
+import { BellIcon, SendIcon } from "../Components/Common/Icon";
 
 const Home = () => {
   return (
     <div>
       <Nav />
+
+      <BellIcon width={"70"} fill={"#fff"} />
+      <SendIcon width={"200"} fill={"#fff"} />
     </div>
   );
 };
